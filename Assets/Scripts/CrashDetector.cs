676c3d9ecbe0e4b9ef259ff004f2f34c3425bcbb
Santirazoqui <irazoqui.santi@gmail.com>
2022-08-09 13:42:09 -0300

    Added finish line and head collision

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            Debug.Log("Ouch!");
+        }
+    }
+}
