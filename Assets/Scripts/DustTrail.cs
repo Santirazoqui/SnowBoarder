b815c9ccf5c039812c8c0828a860f7717f4cdd19
Santirazoqui <irazoqui.santi@gmail.com>
2022-08-19 14:02:25 -0300

    Added dust trail particle effect to player

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class DustTrail : MonoBehaviour
 {
+    [SerializeField] ParticleSystem dustTrail;
+
     private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("swish");
+        if(other.gameObject.tag == "Ground")
+        {
+            dustTrail.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground")
+        {
+            dustTrail.Stop();
+        }
     }
 }
