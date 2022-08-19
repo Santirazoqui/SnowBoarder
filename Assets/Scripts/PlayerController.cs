7adc10a725082e8c69c4e42c9acffb853860ea13
Santirazoqui <irazoqui.santi@gmail.com>
2022-08-19 13:07:00 -0300

    Add boost when pressing up key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed;
+    [SerializeField] float baseSpeed;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RotatePlayer()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,6 +33,18 @@ public class PlayerController : MonoBehaviour
         else if(Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    private void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speedVariation = boostSpeed;
+        } 
+        else 
+        {
+            surfaceEffector2D.speedVariation = baseSpeed;
         }
     }
 }
