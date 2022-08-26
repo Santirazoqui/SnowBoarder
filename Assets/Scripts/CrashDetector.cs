2c23bf9d99880379236f48d66fe0579eb9c4602c
Santirazoqui <irazoqui.santi@gmail.com>
2022-08-26 12:50:31 -0300

    Added sound effects to finish line and crash

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,12 +8,14 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
         if(other.gameObject.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
